7090eb32071da5c2453f30fc7a51b67e2e1ed2c9
henryportillo21 <henry.portillo.ramos@gmail.com>
2021-12-15 10:51:54 -0600

    port 8080

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
-  console.log("The server started on port 3000 !!!!!!");
+app.listen(8080, () => {
+  console.log("The server started on port 8080 !!!!!");
 });
 
 app.get("/", (req, res) => {
